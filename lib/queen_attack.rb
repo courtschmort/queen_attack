796d015d177b9263310e0ca48f97be1486b9192e
Student <example@example.com>
2019-12-09 16:48:48 -0800

    pass all tests.

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,6 +1,18 @@
 class Array
-  def queen_attack?(coordinates)
-    coordinates = [0,0]
-    return false
+  def queen_attack?(pawn)
+    queen_x = self[0]
+    queen_y = self[1]
+    pawn_x = pawn[0]
+    pawn_y = pawn[1]
+
+    if queen_x === pawn_x
+      true
+    elsif queen_y === pawn_y
+      true
+    elsif ((pawn_y - queen_y).abs / (pawn_x - queen_x).abs) === 1
+      true
+    else
+      false
+    end
   end
 end
